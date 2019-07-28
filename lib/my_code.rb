46afcb74e19445e438eb51a8c606b1a83329f5f5
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-28 20:53:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# My Code here....
+def map_to_negativize(source_array)
+  source_array.map { |n| n * -1}
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+  source_array.map { |n| n * 2 }
+end
+
+def map_to_square(source_array)
+  source_array.map { |n| n * n}
+end
+
+def reduce_to_total(source_array,starting_point = 0)
+  source_array.reduce(starting_point) { |sum, n| sum + n }
+end
+
+def reduce_to_all_true(source_array)
+  source_array.all?
+end
+
+def reduce_to_any_true(source_array)
+  source_array.any?
+end
